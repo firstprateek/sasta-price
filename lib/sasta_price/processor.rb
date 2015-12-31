de402559b76245f77dae387059793795c94ab96c
firstprateek <lastprateek@gmail.com>
2015-12-31 22:17:01 +0530

    UI v0.0.2 and bug fix

--- lib/sasta_price/processor.rb
+++ lib/sasta_price/processor.rb
@@ -5,7 +5,7 @@ module SastaPrice
 
 			#flipkart
 			url = "http://www.flipkart.com/search?q=#{CGI.escape(product)}&as=off&as-show=off&otracker=start"
-	    doc = Nokogiri::HTML(HTTParty.get(url).body)  
+	    doc = Nokogiri::HTML(HTTParty.get(url).body)
 	    doc.css('#products .lastUnit').each do |item|
 	      puts "inside"
 	      next if item.at_css('.fk-display-block').blank? || item.at_css('.pu-final .fk-bold').blank?
@@ -15,10 +15,10 @@ module SastaPrice
 	      amount = price[/[0-9\,]+/].gsub(',','').to_i
 	      puts"#{title} #{price}"
 	      append_link = item.at_css('.fk-display-block')[:href] # append http://www.flipkart.com/
-	    	link = "www.flipkart.com#{append_link}"
-	    	response << {title: title, price: price, vendor: "flipkart", link: link, amount: amount}
+	    	link = "http://www.flipkart.com#{append_link}"	
+				response << {title: title, price: price, vendor: "flipkart", link: link, amount: amount}
 	    end
-	    
+
 	    #snapdeal http://www.snapdeal.com/search?keyword=samsung+galaxy+s6&santizedKeyword=moto+g&catId=&categoryId=&suggested=false&vertical=p&noOfResults=20&clickSrc=go_header&lastKeyword=moto+g&prodCatId=&changeBackToAll=false&foundInAll=false&categoryIdSearched=&cityPageUrl=&url=&utmContent=&dealDetail=
 	    url = "http://www.snapdeal.com/search?keyword=#{CGI.escape(product)}&santizedKeyword=&catId=&categoryId=&suggested=false&vertical=&noOfResults=20&clickSrc=go_header&lastKeyword=&prodCatId=&changeBackToAll=false&foundInAll=false&categoryIdSearched=&cityPageUrl=&url=&utmContent=&dealDetail="
 	    doc = Nokogiri::HTML(HTTParty.get(url).body)
@@ -34,7 +34,7 @@ module SastaPrice
 	      link = item.at_css('a')[:href]
 	    	response << {title: title, price: price, vendor: "snapdeal", link: link, amount: amount}
 	    end
-	    
+
 	    #ebay
 	    url = "http://www.ebay.in/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR0.TRC0.H0.X#{CGI.escape(product)}.TRS0&_nkw=#{CGI.escape(product)}&_sacat=0"
 	    doc = Nokogiri::HTML(HTTParty.get(url).body)
@@ -66,7 +66,7 @@ module SastaPrice
 	      link = item.at_css('a')[:href]
 	    	response << {title: title, price: price, vendor: "amazon", link: link, amount: amount}
 	    end
-	   
+
 	    response = sanitize_response(response, product)
 	  end
 
@@ -76,14 +76,14 @@ module SastaPrice
 	  		response = nil
 	  	elsif words.count == 1
 	  		response.reject! { |res| res[:title].downcase.match(product).nil? }
-	  	else 
+	  	else
 	  		match_ratio = (words.count/2.0).ceil
 	  		response.select! do |res|
 	  			value = nil
 	  			words.each_cons(match_ratio) do |cons_word|
 	  				match_string = cons_word.join('.*') + "|" + cons_word.reverse.join('.*')
 	  				if res[:title].downcase.match(match_string).present?
-	  					value = true 
+	  					value = true
 	  					break
 	  				end
 	  			end
@@ -116,7 +116,7 @@ end
 # 		puts "#{match_string}"
 # 		puts "res_title: #{res[:title]}"
 # 		if res[:title].downcase.match(match_string).present?
-# 			value = true 
+# 			value = true
 # 			puts "#{value}"
 # 			break
 # 		end
